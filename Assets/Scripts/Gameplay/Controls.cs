772c82a9c7a93b02978deb2de780c4b8b207f7c2
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2020-05-17 18:37:20 -0500

    Gameplay simple walk and move controls.

--- Assets/Scripts/Gameplay/Controls.cs
+++ Assets/Scripts/Gameplay/Controls.cs
@@ -4,26 +4,41 @@ using UnityEngine;
 
 public class Controls : MonoBehaviour
 {
+    // Script settings
     [SerializeField]
     GameObject cameraRotator;
     [SerializeField]
-    float cameraSpeed = 20.0f;
+    float cameraSpeed = 10.0f;
+    [SerializeField]
+    sbyte cameraXInversion = 1;
+    [SerializeField]
+    sbyte cameraYInversion = 1;
     [SerializeField]
     [Range(0.0f, 360.0f)]
     float cameraMinXRotation = 300.0f;
     [SerializeField]
     [Range(0.0f, 360.0f)]
     float cameraMaxXRotation = 60.0f;
+    [SerializeField]
+    float rotationSpeed = 12.0f;
+    [SerializeField]
+    float walkSpeed = 3.0f;
+
+    // Private properties
+    Animator _animator;
+    bool _playingWalkAnimation = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateControls();
+        UpdateAnimations();
         UpdateCameraRotation();
     }
 
@@ -32,10 +47,65 @@ public class Controls : MonoBehaviour
         UpdateRotatorPosition();
     }
 
+    void UpdateControls()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            MoveAlong(0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            MoveAlong(-180.0f);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            MoveAlong(270.0f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            MoveAlong(90.0f);
+        }
+    }
+
+    void UpdateAnimations()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            if (!_playingWalkAnimation)
+            {
+                _animator.Play("Walk");
+                _playingWalkAnimation = true;
+            }
+        }
+        else
+        {
+            if (_playingWalkAnimation)
+            {
+                _animator.Play("Idle");
+                _playingWalkAnimation = false;
+            }
+        }
+    }
+
+    void MoveAlong(float angle)
+    {
+        var rotation = Quaternion.Euler(
+            transform.eulerAngles.x,
+            cameraRotator.transform.eulerAngles.y + angle,
+            transform.eulerAngles.z
+        );
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+        transform.Translate(Vector3.forward * Time.deltaTime * walkSpeed);
+    }
+
     void UpdateCameraRotation()
     {
-        float inputMouseX = Input.GetAxis("Mouse X");
-        float inputMouseY = Input.GetAxis("Mouse Y");
+        float inputMouseX = Input.GetAxis("Mouse X") * cameraXInversion;
+        float inputMouseY = Input.GetAxis("Mouse Y") * cameraYInversion;
 
         if (inputMouseX != 0.0f)
         {
